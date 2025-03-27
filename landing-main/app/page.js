234b18a104f29aa9668c238e2e43f50ebdea7900
rosilva094 <rosilva094@gmail.com>
2025-03-27 16:25:35 -0300

    Prueba de paje ultra basica

--- landing-main/app/page.js
+++ landing-main/app/page.js
@@ -1,19 +1,6 @@
-import NavBar from './components/NavBar'
-import Banner from './components/Banner'
-import Benefits from './components/Benefits'
-import Examples from './components/Examples'
-import Steps from './components/Steps'
-import Contact from './components/Contact'
-
 export default function Home() {
-  return (
-    <>
-      {<NavBar />}
-      <Banner />
-      <Benefits />
-      <Examples />
-      <Steps />
-      <Contact />
-    </>
-  )
+  return <div style={{ padding: "2rem", textAlign: "center" }}>
+    <h1>¡Landing cargada correctamente!</h1>
+    <p>Si ves esto, el deploy funciona.</p>
+  </div>
 }
